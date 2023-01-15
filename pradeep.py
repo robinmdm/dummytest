499a24b6ffb707c1539054a5c2df8c6f1d541704
MD MOHSIN NUR ROBIN <mrobin132@icloud.com>
2023-01-15 14:03:13 +0100

    update

--- pradeep.py
+++ pradeep.py
@@ -2,6 +2,8 @@
 #   - official python documentation
 #   - w3schools python
 
+#---------------------------------------------------------------------------------------
+
 # Challenge 1:
 
 # # Given a list of names, write a Python program that groups the names that have the same set of characters and returns the grouped names in a list of lists.
@@ -12,6 +14,8 @@
 
 # your code goes here
 
+# ---------------------------------------------------------------------------------------
+
 # Challenge 2:
 
 # # data = [1,[12,'a'],['b','c',4],[5,'d','d'],[50,5]]
@@ -19,6 +23,8 @@
 # # Output: {"a": 50, "b": 12, "c": 5, "d": 4}
 
 # your code goes here
+
+# ------------------------------------------------------------------------------------------
 
 # Challenge 3:
 
@@ -30,6 +36,8 @@
 #           6 8 10
 
 # your code goes here
+
+# ---------------------------------------------------------------------------------------------
 
 # Challenge 4:
 
@@ -58,5 +66,33 @@
 # &       &
 # &        &
 # &         &
+
+# your code goes here
+
+# --------------------------------------------------------------------------------------------------
+
+# challenge 5
+
+# For given number of rows print this pattern
+
+# For input 5, the output will be:
+
+#         1 
+#       1 2 1
+#     1 2 3 2 1
+#   1 2 3 4 3 2 1
+# 1 2 3 4 5 4 3 2 1
+
+# For input 9, the output will be:
+
+#                 1 
+#               1 2 1
+#             1 2 3 2 1
+#           1 2 3 4 3 2 1
+#         1 2 3 4 5 4 3 2 1
+#       1 2 3 4 5 6 5 4 3 2 1
+#     1 2 3 4 5 6 7 6 5 4 3 2 1
+#   1 2 3 4 5 6 7 8 7 6 5 4 3 2 1
+# 1 2 3 4 5 6 7 8 9 8 7 6 5 4 3 2 1
 
 # your code goes here
--- shubham.py
+++ shubham.py
@@ -2,6 +2,8 @@
 #   - official python documentation
 #   - w3schools python
 
+#---------------------------------------------------------------------------------------
+
 # Challenge 1:
 
 # # Given a list of names, write a Python program that groups the names that have the same set of characters and returns the grouped names in a list of lists.
@@ -10,11 +12,19 @@
 
 # # Output: [["PRADEEP", "PRAMIT"], ["ROBIN", "ROLSTAN"], ["SHUBHAM", "SWETHA"]]
 
+# your code goes here
+
+# ---------------------------------------------------------------------------------------
+
 # Challenge 2:
 
 # # data = [1,[12,'a'],['b','c',4],[5,'d','d'],[50,5]]
 
 # # Output: {"a": 50, "b": 12, "c": 5, "d": 4}
+
+# your code goes here
+
+# ------------------------------------------------------------------------------------------
 
 # Challenge 3:
 
@@ -24,6 +34,10 @@
 
 # # output: 3 4 5
 #           6 8 10
+
+# your code goes here
+
+# ---------------------------------------------------------------------------------------------
 
 # Challenge 4:
 
@@ -52,5 +66,33 @@
 # &       &
 # &        &
 # &         &
+
+# your code goes here
+
+# --------------------------------------------------------------------------------------------------
+
+# challenge 5
+
+# For given number of rows print this pattern
+
+# For input 5, the output will be:
+
+#         1 
+#       1 2 1
+#     1 2 3 2 1
+#   1 2 3 4 3 2 1
+# 1 2 3 4 5 4 3 2 1
+
+# For input 9, the output will be:
+
+#                 1 
+#               1 2 1
+#             1 2 3 2 1
+#           1 2 3 4 3 2 1
+#         1 2 3 4 5 4 3 2 1
+#       1 2 3 4 5 6 5 4 3 2 1
+#     1 2 3 4 5 6 7 6 5 4 3 2 1
+#   1 2 3 4 5 6 7 8 7 6 5 4 3 2 1
+# 1 2 3 4 5 6 7 8 9 8 7 6 5 4 3 2 1
 
 # your code goes here
